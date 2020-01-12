c27a1215adfb98d455bd055d7b610e0538367def
Sean Prashad <S.Prashad@outlook.com>
2020-01-12 01:18:28 -0500

    WIP - Add DataGrid component

--- web/src/components/App.js
+++ web/src/components/App.js
@@ -3,11 +3,13 @@ import React from 'react';
 import './styles.scss';
 
 import Navigation from './Navigation';
+import DataGrid from './DataGrid';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
+      <DataGrid />
     </div>
   );
 }
---
+++ web/src/data/index.js
@@ -0,0 +1,11 @@
+const data = [
+  {
+    id: 1,
+    name: 'My Question',
+    url: 'wwww.google.ca',
+    pattern: 'Arrays',
+    companies: ['abc', 'def', 'ghi'],
+  },
+];
+
+export default data;
